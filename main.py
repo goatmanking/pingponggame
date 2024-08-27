5191fbb0c47d238490ac4179a4a6c7258db637f4
mushtaq <75521992+goatmanking@users.noreply.github.com>
2024-08-27 14:11:22 +0500

    New Start and Pause Screen & Sounds

--- main.py
+++ main.py
@@ -9,10 +9,17 @@ running = True
 
 # fonts
 font = pygame.font.Font('./LycheeSoda.ttf', 30)
+start_button_font = pygame.font.Font('./LycheeSoda.ttf', 40)
 font2 = pygame.font.Font('./LycheeSoda.ttf', 64)
-
+Title = pygame.font.Font('./LycheeSoda.ttf', 80)
 # load sound effects
-scored_sound_effect = pygame.mixer.music.load('./scored.ogg')
+scored_sound_effect = pygame.mixer.Sound('./newscored.mp3')
+start_btn_sound = pygame.mixer.Sound('./hit.ogg')
+hit_sound = pygame.mixer.Sound('./energy.mp3')
+enterance_sound = pygame.mixer.Sound('./enterance_sound.mp3')
+puase_soundtrack = pygame.mixer.Sound('pausesoundtrack.mp3')
+boundary_touch_soundeffect = pygame.mixer.Sound('./boundary_touch.mp3')
+boundary_touch_soundeffect_2 = pygame.mixer.Sound('./boundary_touch_2.mp3')
 
 # 'paddle' variables
 PONG_RECT_WIDTH = 10
@@ -33,16 +40,32 @@ X_BALL_SPEED = 6
 ball_surf = pygame.Surface((BALL_WIDTH_N_HEIGHT, BALL_WIDTH_N_HEIGHT))
 ball_surf.fill('white')
 ball_rect = ball_surf.get_frect(center = (1280/2, 720/2))
+animation_ball_rect = ball_surf.get_frect(center= (640, 500))
 left_score = 0
 right_score = 0
+
+# chose random sound
+
+sound_effect_list = ['1', '2']
+
+def chooseRandomBoundarySound(sound: list):
+    choosedSound = random.choice(sound)
+    if choosedSound == '1':
+        boundary_touch_soundeffect.play().set_volume(0.5)
+    elif choosedSound == '2':
+        boundary_touch_soundeffect_2.play()
+
+
 
 
 # bounce ball if it hits the ceiling
 def get_ceiling_ball_collisions(ballsp: float):
     if ball_rect.y + 3 >= 720:
         ballsp *= -1
+        chooseRandomBoundarySound(sound_effect_list)
     elif ball_rect.y - 3 <= 0: 
         ballsp *= -1
+        chooseRandomBoundarySound(sound_effect_list)
     return ballsp
 
 ball_collided = False
@@ -79,7 +102,7 @@ def get_x_collisions():
     if ball_rect.x + 3 >= 1280:
         ball_rect.x = 1280 / 2
         ball_rect.y = 720 / 2
-        pygame.mixer.music.play()
+        scored_sound_effect.play()
         draw_scored(False)
         left_score += 1
         if not ball_collided:
@@ -89,7 +112,7 @@ def get_x_collisions():
         ball_rect.y = 720/2
         right_score += 1
         draw_scored(True)
-        pygame.mixer.music.play()
+        scored_sound_effect.play()
         if not ball_collided: 
             ball_collided = True
 
@@ -102,6 +125,7 @@ def check_ball_collisions(surf):
         if ball_rect.x + BALL_WIDTH_N_HEIGHT >= surf.x:
             X_BALL_SPEED *= -1
             reduction_factor = (surf.height / 2) / MAX_BALL_VELOCITY
+            hit_sound.play()
             difference_in_y = surf.centery - ball_rect.y
             if difference_in_y < 0:
                 difference_in_y *= -1
@@ -117,6 +141,37 @@ def check_ball_collisions(surf):
     else: 
         print("uhhh sussy")
 
+completedDownRun = False
+
+def animatePaddles(rect: pygame.Rect):
+    global completedDownRun
+    if rect.y >= 10 and completedDownRun:
+        rect.y -= 5
+        if rect.top <= 10:
+            completedDownRun = False
+            rect.y += 5
+    elif rect.y <= 720 and not completedDownRun:
+        rect.y += 5
+        if rect.bottom >= 710:
+            completedDownRun = True 
+            rect.y -= 5
+
+def ball_animation_physics(rect):
+
+    global X_BALL_SPEED
+    global Y_BALL_SPEED
+
+    rect.x += X_BALL_SPEED
+    rect.y += Y_BALL_SPEED
+    
+    if rect.y + 3 >= 720:
+        Y_BALL_SPEED *= -1
+    elif rect.y - 3 <= 0: 
+        Y_BALL_SPEED *= -1
+    if rect.x <= 20:
+        X_BALL_SPEED *= -1
+    if rect.x >= 1260:
+        X_BALL_SPEED *= -1
 
 def what_direction():
         rand_choice_list = ["-", "+"]
@@ -128,6 +183,7 @@ def what_direction():
             return False
 
 
+
 pygame.display.set_caption("Ping Pong")
 
 # I do this here as it should run once and then the value should stay the same
@@ -135,7 +191,13 @@ starting_y_direction = what_direction()
 starting_x_direction = what_direction()
 
 
-is_game_paused = False
+is_game_paused = True
+IS_MOUSE_BUTTON_PRESSED= False
+
+start_button_surf = pygame.surface.Surface((200, 50))
+start_button_surf.fill('lightgreen')  
+start_button_rect = start_button_surf.get_frect(center=(640, 400))
+start_btn_font_surf = start_button_font.render('START', True, 'black')
 
 while running:
 
@@ -143,7 +205,10 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            IS_MOUSE_BUTTON_PRESSED = True
+        elif event.type == pygame.MOUSEBUTTONUP:
+            IS_MOUSE_BUTTON_PRESSED = False
     screen.fill("black")
     pygame.draw.rect(screen, 'white', (490, 220, 300, 300), 5)
 
@@ -169,41 +234,78 @@ while running:
     screen.blit(right_score_text, right_score_rect)
 
 
-    Y_BALL_SPEED = get_ceiling_ball_collisions(Y_BALL_SPEED)
-    
-
-
-    if ball_rect.colliderect(white_rect_1):
-        check_ball_collisions(white_rect_1)
-    elif ball_rect.colliderect(white_rect_2):
-        check_ball_collisions(white_rect_2)
-
+   
+   
+    if not is_game_paused:
+        enterance_sound.stop()
+        Y_BALL_SPEED = get_ceiling_ball_collisions(Y_BALL_SPEED)
         
-    if starting_y_direction:
-        ball_rect.y += Y_BALL_SPEED
+
+
+        if ball_rect.colliderect(white_rect_1):
+            check_ball_collisions(white_rect_1)
+        elif ball_rect.colliderect(white_rect_2):
+            check_ball_collisions(white_rect_2)
+
+            
+        if starting_y_direction:
+            ball_rect.y += Y_BALL_SPEED
+        else:
+            ball_rect.y -= Y_BALL_SPEED
+        if starting_x_direction:
+            ball_rect.x += X_BALL_SPEED
+        else: 
+            ball_rect.x -= X_BALL_SPEED
+
+
+        keys = pygame.key.get_pressed()
+        if not  white_rect_1.y <= 7.5:
+            if keys[pygame.K_w]:
+                white_rect_1.y -= PONG_MOVE_SPEED
+        if not white_rect_1.y >= 515:
+            if keys[pygame.K_s]:
+                white_rect_1.y += PONG_MOVE_SPEED
+        if not white_rect_2.y >= 515:
+            if keys[pygame.K_DOWN]:
+                white_rect_2.y += PONG_MOVE_SPEED
+        if not white_rect_2.y <= 7.5:
+            if keys[pygame.K_UP]:
+                white_rect_2.y -= PONG_MOVE_SPEED
+        if keys[pygame.K_p]:
+            is_game_paused = True
     else:
-        ball_rect.y -= Y_BALL_SPEED
-    if starting_x_direction:
-        ball_rect.x += X_BALL_SPEED
-    else: 
-        ball_rect.x -= X_BALL_SPEED
+        # bro just load a button png 
+        screen.fill("black")  
+        mouse_pos = pygame.mouse.get_pos()    
+        screen.blit(ball_surf, animation_ball_rect)
+        screen.blit(start_button_surf, start_button_rect)
+        screen.blit(start_btn_font_surf, (590, 380))
+        pygame.draw.rect(screen, 'white', (540, 376, 200, 50), 4, 3)
+        pygame.draw.rect(screen, 'white', (545, 371, 190, 59), 4, 3)
+        screen.blit(white_surf, white_rect_1)
+        screen.blit(white_surf, white_rect_2)
+        screen.blit(font.render("© Made by Mushtaq", True, 'white'), (540, 660))
+        puase_soundtrack.play()
 
+        if IS_MOUSE_BUTTON_PRESSED:
+            if start_button_rect.collidepoint(mouse_pos):
+                start_btn_sound.play()
+                is_game_paused = False
+        elif start_button_rect.collidepoint(mouse_pos):
+               start_button_surf.set_alpha(230)
+               puase_soundtrack.stop()
+               screen.blit(font2.render('LETS PLAY! :)', True, 'LIGHT GREEN'), (510, 200))
+               enterance_sound.play(1)
+               animatePaddles(white_rect_1)
+               animatePaddles(white_rect_2)
+               ball_animation_physics(animation_ball_rect)
+        else: 
+            enterance_sound.stop()
+            screen.blit(font2.render('NO PLAY NO SLAY! :(', True, 'RED'), (410, 200))    
+            start_button_surf.set_alpha(255)
 
-    keys = pygame.key.get_pressed()
-    if not  white_rect_1.y <= 7.5:
-        if keys[pygame.K_w]:
-            white_rect_1.y -= PONG_MOVE_SPEED
-    if not white_rect_1.y >= 515:
-        if keys[pygame.K_s]:
-            white_rect_1.y += PONG_MOVE_SPEED
-    if not white_rect_2.y >= 515:
-        if keys[pygame.K_DOWN]:
-            white_rect_2.y += PONG_MOVE_SPEED
-    if not white_rect_2.y <= 7.5:
-        if keys[pygame.K_UP]:
-            white_rect_2.y -= PONG_MOVE_SPEED
-        
-    
+        screen.blit(font2.render('THE PING PONG GAME', True, 'white'), (390, 50))
+
     pygame.display.flip()
     clock.tick(60)  
 
